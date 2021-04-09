fde962a592545ea811941058a657605eb0b04b6a
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-04-09 14:44:47 -0500

    handle empty data for plot

--- psychoanalyze/data.py
+++ psychoanalyze/data.py
@@ -4,4 +4,7 @@ import pandas as pd
 
 class WeberFig:
     def __new__(cls, curve_df=None):
-        return px.scatter(curve_df)
+        if curve_df.empty:
+            return px.scatter()
+        else:
+            return px.scatter(curve_df)
--- tests/test_psychoanalyze.py
+++ tests/test_psychoanalyze.py
@@ -3,13 +3,23 @@ from psychoanalyze import data
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+from hypothesis import given, settings
+from hypothesis.strategies import floats
+from hypothesis.extra.pandas import data_frames, columns
 
 
 def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_WeberFig_returns_plotly_figure():
-    # acr_data = pd.DataFrame()
-    fig = data.WeberFig()
+@given(
+    data_frames(
+        columns(
+            names_or_number=["Reference Charge", "Threshold Charge"], elements=floats()
+        )
+    )
+)
+@settings(deadline=None)
+def test_WeberFig_returns_plotly_figure(df):
+    fig = data.WeberFig(df)
     assert isinstance(fig, go.Figure)
