ff8b9429dece96b541693db3e98de179d77a8107
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2021-04-26 21:19:41 -0500

    add pool toggle to dash

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,8 @@
 import dash
 import dash_core_components as dcc
+import dash_daq as daq
 import dash_html_components as html
+from dash.dependencies import Input, Output
 from psychoanalyze.data import Curves, WeberFig
 import pandas as pd
 
@@ -9,7 +11,7 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 curves = Curves(df=pd.read_hdf("data/data.h5", "curves"))
-fig = WeberFig(dim="Amp", curves=curves).plot()
+fig = WeberFig(dim="Amp").plot()
 
 app.layout = html.Div(
     children=[
@@ -19,9 +21,16 @@ app.layout = html.Div(
                 A data visualization dashboard for psychophysics experiments.
             """
         ),
+        daq.BooleanSwitch(id="pool", value=True),
         dcc.Graph(id="weber-plot", figure=fig),
     ]
 )
+
+
+@app.callback(Output("weber-plot", "figure"), Input("pool", "value"))
+def pool_points(pool):
+    return WeberFig("Amp", pool=pool).plot()
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
