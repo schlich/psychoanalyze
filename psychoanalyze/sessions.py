103bfc813b3846c1a5eb47bfaa3e2f0c8317da30
Ty Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-08-04 22:05:06 +0000

    clear files

--- psychoanalyze/sessions.py
+++
@@ -1,5 +0,0 @@
-from typing import List
-
-
-def generate(n: int) -> List[int]:
-    return list(range(n))
--- tests/test_sessions.py
+++
@@ -1,5 +0,0 @@
-import psychoanalyze as pa
-
-
-def test_generate_sessions():
-    assert pa.sessions.generate(3) == [0, 1, 2]
